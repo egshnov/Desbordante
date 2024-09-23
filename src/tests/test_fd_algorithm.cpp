a879e547220f1deca4410a1988228f27497148a7
Ilia B. <nikturab@outlook.com>
2024-09-23 10:48:14 +0000

    Add tests of PFDTane algorithm

--- src/tests/test_fd_algorithm.cpp
+++ src/tests/test_fd_algorithm.cpp
@@ -10,6 +10,7 @@
 #include "algorithms/fd/fun/fun.h"
 #include "algorithms/fd/hyfd/hyfd.h"
 #include "algorithms/fd/pyro/pyro.h"
+#include "algorithms/fd/tane/pfdtane.h"
 #include "algorithms/fd/tane/tane.h"
 #include "model/table/relational_schema.h"
 #include "test_fd_util.h"
